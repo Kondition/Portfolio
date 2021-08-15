af9cb79e347ef9281ca75c0f010b1f912e4dd9b5
Kondition <jonjoeyliew@tutanota.com>
2021-08-15 17:53:25 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Portfolio",
   },
-  plugins: [],
+  plugins: [`gatsby-plugin-postcss`],
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Hero } from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <span>Home</span>
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
